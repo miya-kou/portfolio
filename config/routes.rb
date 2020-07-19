dddeb70bf936b84d9eb094e0ffa1ef407d229bf8
EC2 Default User <ec2-user@ip-172-31-58-157.ec2.internal>
2020-07-19 09:11:10 +0000

    making

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  get 'helps/show'
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  root to: 'tasks#index'
+  get 'signup', to: 'users#new'
+  resources :tasks
+  resources :users, only: [:new, :create]
+end
